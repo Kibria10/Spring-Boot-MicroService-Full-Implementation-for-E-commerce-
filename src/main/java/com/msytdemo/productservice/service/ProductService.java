2f8cb1af36e9d5801a43f6315caa65ef3d956742
Maharab Kibria <junakkibria56@gmail.com>
2023-08-09 20:36:41 +0600

    product service post and get mapping finished

--- src/main/java/com/msytdemo/productservice/service/ProductService.java
+++ src/main/java/com/msytdemo/productservice/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.msytdemo.productservice.service;
 import com.msytdemo.productservice.dto.ProductRequest;
+import com.msytdemo.productservice.dto.ProductResponse;
 import com.msytdemo.productservice.model.Product;
 import com.msytdemo.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,22 @@ public class ProductService {
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build();
-
+        productRepository.save(product);
+        log.info("Product {} saved successfully", product.getId());
     }
-    
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapProductToProductResponse).collect(Collectors.toList());
+    }
+
+    private ProductResponse mapProductToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 
 }
